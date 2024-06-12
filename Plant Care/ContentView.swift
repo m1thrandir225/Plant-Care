8b45320a773fbf622dcb5bb563be7e3bfb2a751f
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-06-12 19:35:17 +0200

    Initial Commit

---
+++ Plant Care/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plant Care
+//
+//  Created by Sebastijan Zindl on 12.6.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plant Care/Plant_CareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_CareApp.swift
+//  Plant Care
+//
+//  Created by Sebastijan Zindl on 12.6.24.
+//
+
+import SwiftUI
+
+@main
+struct Plant_CareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
