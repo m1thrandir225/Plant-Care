0c34da8bbb14461666362e5a5c53104473a1e03c
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-07-24 19:25:58 +0200

    feat: spacing, README & license added

--- Plant Care/Views/AddPlant.swift
+++ Plant Care/Views/AddPlant.swift
@@ -12,14 +12,14 @@ import UserNotifications
 struct AddPlant: View {
 	@Environment (\.modelContext) private var modelContext
 	@Environment (\.presentationMode) var presentationMode
-	
+
 	@State private var plantName = ""
 	@State private var selectedWateringOption: WateringFrequency = .Daily
 	@State private var selectedSunlightOption: SunlightExposure = .PartShade
-	
+
 	@State private var showImagePicker = false
 	@State private var image: UIImage?
-	
+
 	func createPlant() -> UserPlant {
 		return UserPlant(
 			name: plantName,
@@ -28,12 +28,12 @@ struct AddPlant: View {
 			sunlightExposure: selectedSunlightOption
 		)
 	}
-	
+
 	func scheduleNotification(name: String, wateringFrequency: WateringFrequency) async {
 		let content = UNMutableNotificationContent()
 		content.title = "Watering Notification for plant: \(name)"
 		content.body = "Hey, quick reminder, water your plant!"
-		
+
 		var dateComponents = DateComponents()
 		dateComponents.calendar = Calendar.current
 		switch(wateringFrequency) {
@@ -54,15 +54,15 @@ struct AddPlant: View {
 			dateComponents.hour = 8
 			dateComponents.minute = 30
 		}
-		
+
 		let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-		
+
 		let uuidString = UUID().uuidString
-		
+
 		let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
-		
+
 		let notificationCenter = UNUserNotificationCenter.current()
-		
+
 		do {
 			try await notificationCenter.add(request)
 			print("Sucessfully registered request")
@@ -70,17 +70,17 @@ struct AddPlant: View {
 			print("Error registering notification Request")
 		}
 	}
-	
+
     var body: some View {
 		Form {
 			TextField("Plant name", text: $plantName)
-			
+
 			Picker("Watering Frequency", selection: $selectedWateringOption) {
 				ForEach(WateringFrequency.allCases, id: \.self) { option in
 					Text(String(describing: option))
 				}
 			}
-			
+
 			Picker("Sunlight Exposure", selection: $selectedSunlightOption) {
 				ForEach(SunlightExposure.allCases, id: \.self) { option in
 					Text(String(String(describing: option)))
@@ -91,9 +91,9 @@ struct AddPlant: View {
 					.resizable()
 					.scaledToFill()
 					.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: 200).frame(height: 200)
-				
+
 			}
-			
+
 			NavigationLink {
 				ImagePickerView(selectedImage: $image)
 			} label: {
@@ -105,7 +105,7 @@ struct AddPlant: View {
 				})
 			}
 			NavigationLink {
-				CameraView(selectedImage: $image)
+				ImagePickerView(selectedImage: $image)
 			} label: {
 				Button(action: { }, label: {
 					HStack {
@@ -113,7 +113,7 @@ struct AddPlant: View {
 						Text("Take a picture").font(.caption2)
 					}
 				})
-			
+
 			}
 			Button("Add Plant") {
 				UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
@@ -129,17 +129,15 @@ struct AddPlant: View {
 						print(error.localizedDescription)
 					}
 				}
-				
-				
 			}.disabled(plantName.isEmpty || image == nil)
-			
+
 		}.sheet(isPresented: $showImagePicker, content: {
 			CameraView(selectedImage: $image)
 		})
 		.toolbar(.hidden, for: .tabBar)
 		.navigationTitle("Add a new plant")
 		.navigationBarTitleDisplayMode(.inline)
-		
+
     }
 }
 
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 # Plant Care
 
 A iOS app that lets you explore popular plants and their families. As well as it lets you add your own plants and add local notifications to notify you when you need to water them.
+
+---
+
+License MIT © [Sebastijan Zindl](https://sebastijanzindl.me)
